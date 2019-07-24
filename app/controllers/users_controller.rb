7b9b227dff707fd583e1610b3f329dce8a620830
4gotspoo <onehalfgiraffe@gmail.com>
2019-07-24 18:31:28 -0400

    Added will_paginate gem, added pagination to css and to users#index, articles#index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
 	def index
-		@users = User.all
+		@users = User.paginate(page: params[:page], per_page: 1)
 	end
 
 	def new
@@ -34,7 +34,9 @@ class UsersController < ApplicationController
 	end
 
 	def show
-		set_user
+		@users = User.all
+		@user = User.find(params[:id])
+		@user_articles = @user.articles.paginate(page: params[:page], per_page: 3)
 	end
 
 	private
